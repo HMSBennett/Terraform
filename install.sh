3ff0593bd911b0aacab5e9e553e4d31f6970a10c
HMS <harrisonsbennett@gmail.com>
2019-06-27 09:22:53 +0100

    Better versioning

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
+Version="0.12.3"
+
 sudo apt update -y
 
 sudo apt install -y unzip wget
 
-wget https://releases.hashicorp.com/terraform/0.12.3/terraform_0.12.3_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/${Version}/terraform_${Version}_linux_amd64.zip
 
 sudo unzip terraform_*_linux_*.zip
 
